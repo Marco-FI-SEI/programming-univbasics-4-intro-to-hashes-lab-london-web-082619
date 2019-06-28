4741a5b6305f54cf98b88c3c4544cebad578528b
Marco McNulty <mmc90@hotmail.co.uk>
2019-06-28 10:12:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,15 @@ def read_from_hash(hash, key)
   hash[key]
 end
 
+# This should work but fails tests
+# def update_counting_hash(hash, key)
+#   hash[key] ? hash[key] += 1 : hash[key] = 1
+# end
+
 def update_counting_hash(hash, key)
-  hash[key] ? hash[key] += 1 : hash[key] = 1
+  if hash[key].nil?
+    hash[key] = 1
+  else
+    hash[key] += 1
+  end
 end
